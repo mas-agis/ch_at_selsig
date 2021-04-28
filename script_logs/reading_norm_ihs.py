98e85fee1d3ea2a26d8789b35d13726f97e4d244
mas-agis <53692594+mas-agis@users.noreply.github.com>
2021-04-28 13:23:47 +0200

    Set the loop for reading all ihs normalization output and filter rows with score >= 4

--- script_logs/reading_norm_ihs.py
+++ script_logs/reading_norm_ihs.py
@@ -19,20 +19,28 @@ dir_contents
 subs = 'windows'
 # using list comprehension to get string with substring 
 files = [i for i in dir_contents if subs in i]
+#creating empty dataframe to hold all dataset having ihs score >=4
+summary = pd.DataFrame(columns=("start", "end", "ihs_score", "chr", "group"))
 
 #reading each files and concatenate them
-file = 'atfl_29.ihs.out.100bins.norm.10kb.windows'
-#slice the first four letters defining group code
-ras = file[0:4]
-#getting chromosome number by extract string between two strings
-char1, char2 = "_", ".ihs.out"
-chro = file[file.find(char1)+1 : file.find(char2)]
-#reading data using panda
-data = pd.read_csv(file, sep="\t", header=None, usecols=[0,1,5])
-data.columns = ["start", "end", "ihs_score"]
-data["chr"] = chro
-data["group"] = ras
-data
+for file in files:
+    #file = 'atfl_29.ihs.out.100bins.norm.10kb.windows'
+    #slice the first four letters defining group code
+    ras = file[0:4]
+    #set markers for string adjacent to chr number in the file name
+    char1, char2 = "_", ".ihs.out"
+    #getting chromosome number by extract string char1 and char2
+    chro = file[file.find(char1)+1 : file.find(char2)]
+    #reading data using panda
+    data = pd.read_csv(file, sep="\t", header=None, usecols=[0,1,5])
+    #rename columns to match the target summary file
+    data.columns = ["start", "end", "ihs_score"]
+    #adding columns for chr and group
+    data["chr"] = chro
+    data["group"] = ras
+    #append data from the current file which score >=4 to summary data
+    summary = summary.append(data[data["ihs_score"]>= 4])
+
 
 fig, ax = plt.subplots()
 ax.plot(data["start"], data["ihs_score"], "bo" )
