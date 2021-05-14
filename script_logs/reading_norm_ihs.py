c9c31b881fd064b2ae1ccb724dc65f0643b98f72
mas-agis <53692594+mas-agis@users.noreply.github.com>
2021-05-14 10:29:54 +0200

    Update reading_norm_ihs.py

--- script_logs/reading_norm_ihs.py
+++ script_logs/reading_norm_ihs.py
@@ -54,6 +54,20 @@ for file in files:
 g = sns.FacetGrid(data=all_data, col="chr", col_wrap=6, height=2)
 g.map(sns.lineplot, "start", "ihs_score", alpha=0.7)
 
+############################
+#confidence interval
+upper_bound = data["TajimaD"].mean() + (data["TajimaD"].std()*1.5)
+upper_bound
+lower_bound = data["TajimaD"].mean() - (data["TajimaD"].std()*1.5)
+lower_bound
+
+#filtering data greater than upper_bound or lower than lower_bound
+filtered_data = data[data["TajimaD"] > upper_bound]
+filtered_data = filtered_data.append(data[data["TajimaD"] < lower_bound])
+
+###Finding overlaps between summary(iHS) and filtered_data(Tajima's D)
+result = pd.concat([df1, df4], axis=1, join="inner")
+
 ##calculating p-value for every points
 import numpy as np
 from scipy.stats import norm
@@ -85,7 +99,7 @@ probability1
 stats.zscore(probability)
 stats.zscore(probability1)
 
-###Reading contents of Tajima's D
+###Reading contents of each statistical test output
 #import libraries
 import os
 import pandas as pd
@@ -93,7 +107,9 @@ import numpy as np
 import seaborn as sns
 from scipy.stats import norm
 from scipy import stats
+from sklearn import preprocessing
 
+###Reading contents of Tajima's D
 #set working directory
 os.chdir(r"D:\maulana\third_project\D'_statistics\atfl")
 
@@ -102,35 +118,26 @@ file = "atfl_29.Tajima.D"
 data = pd.read_csv(file, sep="\t")
 data.describe()
 
+#create window column by adding 1 to bin-start, to match scanning window of other tests
+data = data.assign(window = lambda x: data["BIN_START"] + 1)
+
 #plot distribution
 sns.lineplot("BIN_START", "TajimaD", data=data)
 sns.distplot(data["TajimaD"])
 
+#standardized score
+X = data["TajimaD"]
+data["standard_TajimaD"] = preprocessing.scale(X)
+
 #getting probability for each point in normal distribution
-data["pval"] = norm.pdf(data["TajimaD"], loc=np.mean(data["TajimaD"]), 
-                     scale=np.std(data["TajimaD"])) #* interval
-data["pval1"] = norm.pdf(data["TajimaD"]) #* interval
+data["pval_tajima"] = norm.pdf(data["TajimaD"]) #* interval
+data["pval_std_tajima"] = norm.pdf(data["standard_TajimaD"] ) #* interval
 
-sns.distplot(data["pval"])
-sns.distplot(data["pval1"])
-#confidence interval
-upper_bound = data["TajimaD"].mean() + (data["TajimaD"].std()*1.5)
-upper_bound
-lower_bound = data["TajimaD"].mean() - (data["TajimaD"].std()*1.5)
-lower_bound
-
-#filtering data greater than upper_bound or lower than lower_bound
-filtered_data = data[data["TajimaD"] > upper_bound]
-filtered_data = filtered_data.append(data[data["TajimaD"] < lower_bound])
-
-###Finding overlaps between summary(iHS) and filtered_data(Tajima's D)
-result = pd.concat([df1, df4], axis=1, join="inner")
+#sns.distplot(data["pval"])
+sns.distplot(data["pval_tajima"])
+sns.distplot(data["pval_std_tajima"])
 
 ###Read output of Fst fst atfl_chya chr_29 
-import os
-import pandas as pd
-import seaborn as sns
-
 #set working directory
 os.chdir(r"D:\maulana\third_project\fst\atfl")
 
@@ -142,6 +149,14 @@ data1.describe()
 #plot distribution
 sns.lineplot("BIN_START", "MEAN_FST", data=data1)
 sns.distplot(data1["MEAN_FST"])
+
+#standardized score
+X = data1["MEAN_FST"]
+data1["standard_fst"] = preprocessing.scale(X)
+
+#getting probability for each point in normal distribution
+data1["pval_fst"] = norm.pdf(data1["MEAN_FST"]) #* interval
+data1["pval_std_fst"] = norm.pdf(data1["standard_fst"]) #* interval
 
 ###Reading iHS normalization
 os.chdir(r"D:\maulana\third_project\norm_ihs\atfl")
@@ -155,6 +170,9 @@ data2.describe()
 sns.lineplot(data2[0], data2[5])
 sns.distplot(data2[5])
 
+#getting probability for each point in normal distribution
+data2["pval_ihs"] = norm.pdf(data2[5]) #* interval
+
 ###Reading nSL normalization
 os.chdir(r"D:\maulana\third_project\norm_nsl\atfl")
 
@@ -166,6 +184,9 @@ data3.describe()
 #plot distribution
 sns.lineplot(data3[0], data3[5])
 sns.distplot(data3[5])
+
+#getting probability for each point in normal distribution
+data3["pval_nsl"] = norm.pdf(data3[5]) #* interval
 
 ###Reading normalization xpehh 
 os.chdir(r"D:\maulana\third_project\norm_xpehh\atfl")
@@ -179,6 +200,34 @@ data4.describe()
 sns.lineplot(data4[0], data4[8])
 sns.distplot(data4[8])
 
+#getting probability for each point in normal distribution
+data4["pval_xpehh"] = norm.pdf(data4[8]) #* interval
+
+#transform probability to z-score - both un/normalized have same z-score
+data["z"] = stats.zscore(data["pval"], nan_policy="omit")
+data["z1"] = stats.zscore(data["pval1"], nan_policy="omit")
+
 ##making class with inputs file, which chromosomes, column of the windows/pos, 
 # column of the value(default the last column of daata), 
 #option whether value is p-value or not(default is yes) (5 arguments)
+
+#data["pval"] = norm.pdf(data["TajimaD"], loc=np.mean(data["TajimaD"]), 
+               #      scale=np.std(data["TajimaD"])) #* interval
+
+#data normalization
+temp = data["TajimaD"].to_numpy() 
+norm = preprocessing.normalize(temp.reshape(-1, 1))
+
+# Standardize the data attributes for the Iris dataset.
+from sklearn.datasets import load_iris
+from sklearn import preprocessing
+# load the Iris dataset
+iris = load_iris()
+print(iris.data.shape)
+# separate the data and target attributes
+X = iris.data
+y = iris.target
+# standardize the data attributes
+standardized_X = preprocessing.scale(X)
+
+preprocessing.normalize(data["pval1"] )
